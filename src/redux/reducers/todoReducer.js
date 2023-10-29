f4e4855552e6646ed9abd56890f9903926d2c02a
suryaprmdya <suryaoktova.p@gmail.com>
2023-10-29 14:18:27 +0700

    fetching data API

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,19 +1,40 @@
+import axios from "axios";
+
 const intialState = {
-    todos: [
-        {id: 1, title: "Todo 1", isFinish: false},
-        {id: 2, title: "Todo 2", isFinish: false},
-        {id: 3, title: "Todo 3", isFinish: false}
-    ],
-    isLoading: false,
-    error: ""
+  todos: [],
+  isLoading: false,
+  error: "",
+};
+
+function todoReducer(state = intialState, action) {
+  switch (action.type) {
+    case "SUCCESS_GET_TODO":
+      return {
+        ...state,
+        isLoading: false,
+        todos: action.payload,
+      };
+
+    default:
+      return state;
+  }
 }
 
-function todoReducer(state=intialState, action) {
-    switch (action) {
-        default:
-            return state
-    }
+function successGetTodo(data) {
+  return {
+    type: "SUCCESS_GET_TODO",
+    payload: data,
+  };
 }
 
+export function getTodo() {
+  return async function (dispatch) {
 
-export default todoReducer
+    const url = "https://652f72f90b8d8ddac0b27cb8.mockapi.io/todo";
+    const {data} = await axios(url);
+
+    dispatch(successGetTodo(data));
+  };
+}
+
+export default todoReducer;
